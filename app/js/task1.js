02dd7fdd84591f10be8fb879a444bf0d5052796a
Ekaterina Mozheiko <ek.mozheiko@gmail.com>
2018-04-07 14:21:15 +0700

    task1-js

---
+++ app/js/task1.js
@@ -0,0 +1,5 @@
+var elements = [];
+elements = document.getElementsByTagName("*");
+var randElement = elements[Math.floor(Math.random() * elements.length)];
+randElement.style.backgroundColor = "red";
+
