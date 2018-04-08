0d1404979ec4ca87c5d513d5181a798762ce1984
Ekaterina Mozheiko <ek.mozheiko@gmail.com>
2018-04-08 13:03:34 +0700

    JStask1_2

--- app/js/task1.js
+++ app/js/task1.js
@@ -1,6 +1,14 @@
-const elements = document.getElementsByTagName("*");
-var randElement = elements[Math.floor(Math.random() * elements.length)];
-randElement.style.backgroundColor = "red";
+const elements = document.getElementsByTagName('*'),
+	TIME_INTERVAL = 2000;
+
+function getRandomElement(elementsArr) {
+	var randomElement = elementsArr[Math.floor(Math.random() * elementsArr.length)];
+	var randomColor = '#' + Math.random().toString(16).slice(-6);
+	randomElement.style.backgroundColor = randomColor;
+	return;
+}
+
+var timeId = setInterval('getRandomElement(elements)', TIME_INTERVAL);
 
 
 
