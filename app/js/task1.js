5871584408dcc0523187a70da11aea8ed52b3eaf
Ekaterina Mozheiko <ek.mozheiko@gmail.com>
2018-04-10 13:21:49 +0700

    Add some changes to Task1 JS

--- app/js/task1.js
+++ app/js/task1.js
@@ -1,24 +1,28 @@
 const elements = document.getElementsByTagName('*'), 
-	TIME_INTERVAL = 2000;
+	TIME_INTERVAL_SET = 2000,
+	TIME_INTERVAL_CLEAR = 2000;
+
+var storage = [];
 
 function getRandomElement(elementsArr) {
-	return randomElement = elementsArr[Math.floor(Math.random() * elementsArr.length)];
+	var randomElement = elementsArr[Math.floor(Math.random() * elementsArr.length)];
+	storage.push(randomElement);
+	return randomElement;
 }
 
 function setBackground() {
 	var randomElement = getRandomElement(elements)
 	var randomColor = '#' + Math.random().toString(16).slice(-6);
 	randomElement.style.backgroundColor = randomColor;
-	return;
 }
 
 function clearBackground() {
-	var randomElement = getRandomElement(elements);
-	randomElement.style.backgroundColor = 'transparent';
+	var randomElement = getRandomElement(storage);
+	randomElement.style.backgroundColor = '';
 }
 
-var timeId = setInterval('setBackground()', TIME_INTERVAL);
-var timeId2 = setInterval('clearBackground()', TIME_INTERVAL);
+var timeId_set = setInterval(setBackground, TIME_INTERVAL_SET);
+var timeId_clear = setInterval(clearBackground, TIME_INTERVAL_CLEAR);
 
 
 
